d4f2636b46a229ce750ba174858391e728248b84
Mithun Kumar BM <163859532+Mith0604@users.noreply.github.com>
2025-03-22 17:49:58 +0530

    firebase-config.js

--- gymapp/public/js/firebase-config.js
+++ gymapp/public/js/firebase-config.js
@@ -2,13 +2,13 @@
 
 // Your Firebase configuration (copy/paste your provided values)
 const firebaseConfig = {
-  apiKey: "AIzaSyCMFc6DsxT5SDv9jhAC9mg3LygNmjN7FPc",
-  authDomain: "gym-management-9f7ca.firebaseapp.com",
-  projectId: "gym-management-9f7ca",
-  storageBucket: "gym-management-9f7ca.firebasestorage.app",
-  messagingSenderId: "513873597591",
-  appId: "1:513873597591:web:21f4ba16f8775f586c45d2",
-  measurementId: "G-SLC4ZBW3HX"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
 };
 
 // Initialize Firebase
